a3061596bcff36400394604602a38e292a697495
nyung517 <k.youngbin.001@gmail.com>
2026-05-29 22:34:45 +0900

    rm: create_test_db.sh

--- create_test_db.sh
+++
@@ -1,8 +0,0 @@
-podman run -d \
-  --name ebazi-db-test \
-  -e POSTGRES_USER=user \
-  -e POSTGRES_PASSWORD=wasans \
-  -e POSTGRES_DB=main \
-  -p 5432:5432 \
-  -v testdata:/var/lib/postgresql \
-  postgres:latest
