7da94a6be94e2ea83837b5466da249bcbe218f55
rhtnddd <qlalfql123@gmail.com>
2026-06-11 01:26:16 +0900

    add: UserRow가 UserTable에서 꺼낸 데이터 코드를 쓸 수 있는 형태로 추가

--- src/main/kotlin/com/github/nepyh/rooter/module/user/model/UserRow.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/user/model/UserRow.kt
@@ -1,2 +1,13 @@
 package com.github.nepyh.rooter.module.user.model
 
+import java.time.LocalDateTime
+
+data class UserRow(
+    val id: Int,
+    val email: String,
+    val userName: String,
+    val password: String,
+    val avatarImageKey: String?,
+    val bio: String?,
+    val createdAt: LocalDateTime
+)
