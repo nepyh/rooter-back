cc85607e7e6d2fe9b07acc42b324c0deb6a9ab00
rhtnddd <qlalfql123@gmail.com>
2026-06-11 00:51:04 +0900

    add: UserModule.kt Koin에 UserRepo, UserService, UserAuthService 등록하고 라우팅 연결

--- src/main/kotlin/com/github/nepyh/rooter/module/user/UserModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/user/UserModule.kt
@@ -1,2 +1,19 @@
 package com.github.nepyh.rooter.module.user
 
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import org.koin.dsl.module
+
+val userModule = module {
+    single { UserRepo() }
+    single { UserService(get()) }
+    single { UserAuthService(get(), get()) }
+}
+
+fun Application.installUserModule() {
+    routing {
+        route("/user") {
+            configureUserAPI()
+        }
+    }
+}
