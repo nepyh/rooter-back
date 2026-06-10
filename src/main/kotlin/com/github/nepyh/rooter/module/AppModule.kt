e2a1c19515438fc03de33e9128630797a5046064
rhtnddd <qlalfql123@gmail.com>
2026-06-11 00:56:22 +0900

    add: AppModule.kt에 유저DI모듈과 유저Route모듈 합침

--- src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
@@ -3,14 +3,18 @@ package com.github.nepyh.rooter.module
 import com.github.nepyh.rooter.module.blink.blinkModule
 import com.github.nepyh.rooter.module.blink.installBlinkModule
 import com.github.nepyh.rooter.module.health.installHealthModule
+import com.github.nepyh.rooter.module.user.userModule
+import com.github.nepyh.rooter.module.user.installUserModule
 import io.ktor.server.application.Application
 import org.koin.dsl.module
 
 val appModule = module {
     includes(blinkModule)
+    includes(userModule)
 }
 
 fun Application.installAppModule() {
     installBlinkModule()
     installHealthModule()
+    installUserModule()
 }
