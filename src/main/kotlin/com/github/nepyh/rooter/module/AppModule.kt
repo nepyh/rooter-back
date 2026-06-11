2bb31d18db9351b51896b7f7904987cf4fe3cf38
nyung517 <k.youngbin.001@gmail.com>
2026-06-11 22:47:21 +0900

    edit: 모든 백엔드 api 가 /api 경로부터 시작하도록 configureAppModule 의 라우트 설정 부분을 수정

--- src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
@@ -3,6 +3,7 @@ package com.github.nepyh.rooter.module
 import com.github.nepyh.rooter.common.ApiRoute
 import com.github.nepyh.rooter.module.example.exampleModule
 import io.ktor.server.application.*
+import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import org.koin.dsl.module
 import org.koin.ktor.ext.inject
@@ -16,8 +17,10 @@ fun Application.configureAppModule() {
     val apiRoutes by inject<List<ApiRoute>>()
 
     routing {
-        apiRoutes.forEach { apiRoute ->
-            with(apiRoute) { configureRoute() }
+        route("/api") {
+            apiRoutes.forEach { apiRoute ->
+                with(apiRoute) { configureRoute() }
+            }
         }
     }
 }
