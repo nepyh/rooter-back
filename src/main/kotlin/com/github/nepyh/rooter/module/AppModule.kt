e5a27e34f1d9911342f8236ae34e9e255969a0c1
nyung517 <k.youngbin.001@gmail.com>
2026-06-11 21:57:53 +0900

    edit: configureAppModule 의 로직을 각 모듈의 설치 함수 호출이 아닌, api 유닛을 설치하는 로직으로 변경

--- src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
@@ -1,16 +1,22 @@
 package com.github.nepyh.rooter.module
 
+import com.github.nepyh.rooter.common.ApiRoute
 import com.github.nepyh.rooter.module.blink.blinkModule
-import com.github.nepyh.rooter.module.blink.installBlinkModule
-import com.github.nepyh.rooter.module.health.installHealthModule
-import io.ktor.server.application.Application
+import io.ktor.server.application.*
+import io.ktor.server.routing.routing
 import org.koin.dsl.module
+import org.koin.ktor.ext.inject
 
 val appModule = module {
     includes(blinkModule)
 }
 
 fun Application.configureAppModule() {
-    installBlinkModule()
-    installHealthModule()
+    val apiRoutes by inject<List<ApiRoute>>()
+
+    routing {
+        apiRoutes.forEach { apiRoute ->
+            with(apiRoute) { configureRoute() }
+        }
+    }
 }
