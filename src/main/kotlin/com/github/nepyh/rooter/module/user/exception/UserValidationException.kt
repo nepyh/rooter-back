7d11145c4923ed0312b6feda0cc4017ed764c47d
rhtnddd <qlalfql123@gmail.com>
2026-06-11 16:42:49 +0900

    add: UserValidationException에 유저 입력값 검증 실패 관련 예외 기능 추가

--- src/main/kotlin/com/github/nepyh/rooter/module/user/exception/UserValidationException.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/user/exception/UserValidationException.kt
@@ -1,2 +1,8 @@
 package com.github.nepyh.rooter.module.user.exception
 
+sealed class UserValidationException(message: String) : Exception(message) {
+    class DuplicatedEmailException : UserValidationException("이미 사용 중인 이메일입니다.")
+    class WrongPasswordFormatException : UserValidationException("비밀번호 형식이 올바르지 않습니다.")
+    class WrongPasswordException : UserValidationException("비밀번호가 일치하지 않습니다.")
+    class WrongUserNameException : UserValidationException("사용할 수 없는 사용자 이름입니다.")
+}
