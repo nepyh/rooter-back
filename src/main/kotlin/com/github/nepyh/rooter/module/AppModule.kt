a6d5d10e65e66a9674d81bd004fda730a9069df0
nyung517 <k.youngbin.001@gmail.com>
2026-06-11 23:45:30 +0900

    fix: appModule 부에 ApiRoute 의 목록을 가져오는 싱글톤을 추가하여 configureAppModule 시에 라우트의 목록을 가져오지 못하는 문제 해결

--- src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
@@ -3,14 +3,15 @@ package com.github.nepyh.rooter.module
 import com.github.nepyh.rooter.common.ApiRoute
 import com.github.nepyh.rooter.module.example.exampleModule
 import io.ktor.server.application.*
-import io.ktor.server.routing.route
-import io.ktor.server.routing.routing
+import io.ktor.server.routing.*
 import org.koin.dsl.module
 import org.koin.ktor.ext.inject
 
 
 val appModule = module {
     includes(exampleModule)
+
+    single<List<ApiRoute>> { getAll() }
 }
 
 fun Application.configureAppModule() {
