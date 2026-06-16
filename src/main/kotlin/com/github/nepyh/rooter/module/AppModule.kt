7817ade3be0b94984bb32e051138520a96d366c9
rhtnddd <qlalfql123@gmail.com>
2026-06-17 07:34:35 +0900

    edit: 개별 이름표를 하나씩 명시해서 get()으로 쏙쏙 뽑아오는 방식으로 변경

--- src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
@@ -1,16 +1,35 @@
 package com.github.nepyh.rooter.module
 
-import com.github.nepyh.rooter.module.blink.blinkModule
-import com.github.nepyh.rooter.module.blink.installBlinkModule
-import com.github.nepyh.rooter.module.health.installHealthModule
-import io.ktor.server.application.Application
+import com.github.nepyh.rooter.common.ApiRoute
+import com.github.nepyh.rooter.module.example.exampleModule
+import com.github.nepyh.rooter.module.health.healthModule
+import com.github.nepyh.rooter.module.user.userModule
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import org.koin.ktor.ext.getKoin
 
 val appModule = module {
-    includes(blinkModule)
+    includes(exampleModule)
+    includes(healthModule)
+    includes(userModule)
 }
 
-fun Application.installAppModule() {
-    installBlinkModule()
-    installHealthModule()
+fun Application.configureAppModule() {
+    routing {
+        route("api") {
+            val koin = getKoin()
+
+            val routeNames = listOf("apiRoute", "exampleApi")
+
+            val apiRoutes = routeNames.map { name ->
+                koin.get<ApiRoute>(named(name))
+            }
+
+            apiRoutes.forEach { apiRoute ->
+                with(apiRoute) { configureRoute() }
+            }
+        }
+    }
 }
