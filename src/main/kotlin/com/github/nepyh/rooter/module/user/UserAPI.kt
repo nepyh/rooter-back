04e3f6eadb7446807297cbe673c76b9ea95e72a5
rhtnddd <qlalfql123@gmail.com>
2026-06-16 14:22:02 +0900

    edit: inject방식에서 example 패키지 패턴으로 변경

--- src/main/kotlin/com/github/nepyh/rooter/module/user/UserAPI.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/user/UserAPI.kt
@@ -1,5 +1,6 @@
 package com.github.nepyh.rooter.module.user
 
+import com.github.nepyh.rooter.common.ApiRoute
 import com.github.nepyh.rooter.module.user.dto.UserLoginRequest
 import com.github.nepyh.rooter.module.user.dto.UserRegisterRequest
 import com.github.nepyh.rooter.module.user.exception.UserNotFoundException
@@ -8,10 +9,9 @@ import io.ktor.http.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.koin.ktor.ext.inject
 
-fun Route.configureUserAPI() {
-    post {
+fun UserApi(userService: UserService) = ApiRoute("users") {
+    post("") {
         try {
             val request = call.receive<UserRegisterRequest>()
             val response = userService.registerUser(request)
@@ -20,16 +20,14 @@ fun Route.configureUserAPI() {
             call.respond(HttpStatusCode.Conflict, mapOf("message" to e.message))
         } catch (e: UserValidationException.WrongPasswordFormatException) {
             call.respond(HttpStatusCode.BadRequest, mapOf("message" to e.message))
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             call.respond(HttpStatusCode.InternalServerError, mapOf("message" to "서버 오류가 발생했습니다."))
         }
     }
 }
 
-fun Route.configureAuthAPI() {
-    val userAuthService: UserAuthService by inject()
-
-    post("/login") {
+fun AuthApi(userAuthService: UserAuthService) = ApiRoute("auth") {
+    post("login") {
         try {
             val request = call.receive<UserLoginRequest>()
             val response = userAuthService.login(request)
@@ -38,7 +36,7 @@ fun Route.configureAuthAPI() {
             call.respond(HttpStatusCode.NotFound, mapOf("message" to e.message))
         } catch (e: UserValidationException.WrongPasswordException) {
             call.respond(HttpStatusCode.Unauthorized, mapOf("message" to e.message))
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             call.respond(HttpStatusCode.InternalServerError, mapOf("message" to "서버 오류가 발생했습니다."))
         }
     }
