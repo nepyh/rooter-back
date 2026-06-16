984ba88f5ed9584f8c0f8d027970b618da81ebbd
rhtnddd <qlalfql123@gmail.com>
2026-06-17 07:30:49 +0900

    rm: 겹치는 의존성 제거(exposed-jdbc)

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
 
     implementation("org.jetbrains.exposed:exposed-core:${exposedVersion}")
     implementation("org.jetbrains.exposed:exposed-dao:${exposedVersion}")
-    implementation("org.jetbrains.exposed:exposed-jdbc:${exposedVersion}")
     implementation("org.jetbrains.exposed:exposed-java-time:${exposedVersion}")
     implementation("org.mindrot:jbcrypt:0.4")
     // exposed jdbc driver using different version name
