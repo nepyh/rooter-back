b160b039fc4bef41d3a0310e7bef5f0e0fa9ae9d
nyung517 <k.youngbin.001@gmail.com>
2026-06-18 08:04:33 +0900

    add: 이유는 모르겠는데 삭제된 진입점 모듈 Application.prodModule 다시 복구

--- src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
+++ src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
@@ -12,40 +12,6 @@ import io.ktor.server.plugins.cors.routing.*
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
-fun Application.prodModule() {
-    install(Koin) {
-        slf4jLogger()
-        modules(appModule)
-    }
-
-    install(ContentNegotiation) {
-        json()
-    }
-
-    install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Get)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader(HttpHeaders.ContentType)
-        anyHost()
-    }
-
-    serverConfig { developmentMode = false }
-
-    DatabaseManager.init(
-        DatabaseConfig(
-            driverClassName = "org.postgresql.Driver",
-            jdbcUrl = environment.config.property("storage.jdbcUrl").getString(),
-            username = environment.config.property("storage.dbUser").getString(),
-            password = environment.config.property("storage.dbPassword").getString(),
-            maxPoolSize = environment.config.property("storage.dbMaxPoolSize").getString().toInt()
-        )
-    )
-    configureAppModule()
-}
 
 fun Application.devModule() {
     install(Koin) {
@@ -70,6 +36,44 @@ fun Application.devModule() {
 
     serverConfig {
         developmentMode = true
+    }
+
+    DatabaseManager.init(
+        DatabaseConfig(
+            driverClassName = "org.postgresql.Driver",
+            jdbcUrl = environment.config.property("storage.jdbcUrl").getString(),
+            username = environment.config.property("storage.dbUser").getString(),
+            password = environment.config.property("storage.dbPassword").getString(),
+            maxPoolSize = environment.config.property("storage.dbMaxPoolSize").getString().toInt()
+        )
+    )
+    configureAppModule()
+}
+
+
+fun Application.prodModule() {
+    install(Koin) {
+        slf4jLogger()
+        modules(appModule)
+    }
+
+    install(ContentNegotiation) {
+        json()
+    }
+
+    install(CORS) { // TODO env!!!!!!!!!
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.ContentType)
+        anyHost()
+    }
+
+    serverConfig {
+        developmentMode = false
     }
 
     DatabaseManager.init(
