699385981146241218ee6035cbb07fe4e27b83a0
rhtnddd <qlalfql123@gmail.com>
2026-06-17 10:32:57 +0900

    add: prodModule추가

--- src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
+++ src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
@@ -12,6 +12,41 @@ import io.ktor.server.plugins.cors.routing.*
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
+fun Application.prodModule() {
+    install(Koin) {
+        slf4jLogger()
+        modules(appModule)
+    }
+
+    install(ContentNegotiation) {
+        json()
+    }
+
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.ContentType)
+        anyHost()
+    }
+
+    serverConfig { developmentMode = false }
+
+    DatabaseManager.init(
+        DatabaseConfig(
+            driverClassName = "org.postgresql.Driver",
+            jdbcUrl = environment.config.property("storage.jdbcUrl").getString(),
+            username = environment.config.property("storage.dbUser").getString(),
+            password = environment.config.property("storage.dbPassword").getString(),
+            maxPoolSize = environment.config.property("storage.dbMaxPoolSize").getString().toInt()
+        )
+    )
+    configureAppModule()
+}
+
 fun Application.devModule() {
     install(Koin) {
         slf4jLogger()
