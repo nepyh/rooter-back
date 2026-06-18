ac9f5fe0de11fb45dc7c122411111f398ac71ffb
nyung517 <k.youngbin.001@gmail.com>
2026-06-19 02:31:47 +0900

    add: AppConfig 에 FileStorage 관련 설정값 추가

--- src/main/kotlin/com/github/nepyh/rooter/config/AppConfig.kt
+++ src/main/kotlin/com/github/nepyh/rooter/config/AppConfig.kt
@@ -14,7 +14,12 @@ data class AppConfig(
 
     // cors related
     val corsAllowedHosts: List<String>,
-    val corsMaxAgeSeconds: Long
+    val corsMaxAgeSeconds: Long,
+
+    // storage related
+    val storageType: String,
+    val storageBaseDir: String?,
+    val storageBaseUrl: String?
 ) {
     companion object {
         fun fromApplicationConfig(config: ApplicationConfig): AppConfig {
@@ -43,7 +48,11 @@ data class AppConfig(
                 dbMaxPoolSize = config.property("database.dbMaxPoolSize").getString().toInt(),
 
                 corsAllowedHosts = allowedHosts,
-                corsMaxAgeSeconds = config.property("cors.maxAgeSeconds").getString().toLong()
+                corsMaxAgeSeconds = config.property("cors.maxAgeSeconds").getString().toLong(),
+
+                storageType = config.property("storage.type").getString(),
+                storageBaseDir = config.propertyOrNull("storage.baseDir")?.getString(),
+                storageBaseUrl = config.propertyOrNull("storage.baseUrl")?.getString()
             )
         }
     }
