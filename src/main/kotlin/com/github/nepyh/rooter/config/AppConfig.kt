81559a7dbb4e1745fe15ae7e14683693f8c0d0da
nyung517 <k.youngbin.001@gmail.com>
2026-06-19 00:28:02 +0900

    rm: 사용되지 않는 주석 코드 제거 (AppConfig.kt)

--- src/main/kotlin/com/github/nepyh/rooter/config/AppConfig.kt
+++ src/main/kotlin/com/github/nepyh/rooter/config/AppConfig.kt
@@ -2,13 +2,6 @@ package com.github.nepyh.rooter.config
 
 import io.ktor.server.config.ApplicationConfig
 
-/*
-
-            jdbcUrl = environment.config.property("storage.jdbcUrl").getString(),
-            username = environment.config.property("storage.dbUser").getString(),
-            password = environment.config.property("storage.dbPassword").getString(),
-            maxPoolSize = environment.config.property("storage.dbMaxPoolSize").getString().toInt()
- */
 
 data class AppConfig(
     val environment: EnvironmentMode,
