f621cbc0bcaf469accc21c803821aa999d71697e
nyung517 <k.youngbin.001@gmail.com>
2026-06-19 00:26:00 +0900

    edit: Application.appEntryModule internal code is edited to fit to new AppConfig class and AppModule factory function

--- src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
+++ src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
@@ -1,8 +1,8 @@
 package com.github.nepyh.rooter
 
-import com.github.nepyh.rooter.module.appModule
-import com.github.nepyh.rooter.module.database.DatabaseConfig
-import com.github.nepyh.rooter.module.database.DatabaseManager
+import com.github.nepyh.rooter.config.AppConfig
+import com.github.nepyh.rooter.config.EnvironmentMode
+import com.github.nepyh.rooter.module.AppModule
 import com.github.nepyh.rooter.module.configureAppModule
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
@@ -12,15 +12,13 @@ import io.ktor.server.plugins.cors.routing.*
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
-/**
- * TODO CROS 세팅은 env 로 따로 넣어줘야됨. 장기적으로 anyHost 에서 env 로 바꿔야됨
- * 지금은 그냥 이렇게 하고 나중에 AppConfig 같은게 좀 정리가 되면 그떄 env 로 분리하는걸로
- */
 
-fun Application.devModule() {
+fun Application.appEntryModule() {
+    val appConfig = AppConfig.fromApplicationConfig(environment.config)
+
     install(Koin) {
         slf4jLogger()
-        modules(appModule)
+        modules(AppModule(appConfig))
     }
 
     install(ContentNegotiation) {
@@ -35,59 +33,21 @@ fun Application.devModule() {
         allowMethod(HttpMethod.Delete)
         allowHeader(HttpHeaders.Authorization)
         allowHeader(HttpHeaders.ContentType)
-        anyHost()
+
+        if (appConfig.environment == EnvironmentMode.PROD) {
+            appConfig.corsAllowedHosts.forEach { hostName ->
+                allowHost(hostName, schemes = listOf("http", "https"))
+            }
+        } else {
+            anyHost()
+        }
+
+        allowCredentials = true
     }
 
     serverConfig {
-        developmentMode = true
+        developmentMode = appConfig.environment == EnvironmentMode.DEV
     }
 
-    DatabaseManager.init(
-        DatabaseConfig(
-            driverClassName = "org.postgresql.Driver",
-            jdbcUrl = environment.config.property("storage.jdbcUrl").getString(),
-            username = environment.config.property("storage.dbUser").getString(),
-            password = environment.config.property("storage.dbPassword").getString(),
-            maxPoolSize = environment.config.property("storage.dbMaxPoolSize").getString().toInt()
-        )
-    )
-    configureAppModule()
-}
-
-
-fun Application.prodModule() {
-    install(Koin) {
-        slf4jLogger()
-        modules(appModule)
-    }
-
-    install(ContentNegotiation) {
-        json()
-    }
-
-    install(CORS) { // TODO env!!!!!!!!!
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Get)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader(HttpHeaders.ContentType)
-        anyHost()
-    }
-
-    serverConfig {
-        developmentMode = false
-    }
-
-    DatabaseManager.init(
-        DatabaseConfig(
-            driverClassName = "org.postgresql.Driver",
-            jdbcUrl = environment.config.property("storage.jdbcUrl").getString(),
-            username = environment.config.property("storage.dbUser").getString(),
-            password = environment.config.property("storage.dbPassword").getString(),
-            maxPoolSize = environment.config.property("storage.dbMaxPoolSize").getString().toInt()
-        )
-    )
     configureAppModule()
 }
