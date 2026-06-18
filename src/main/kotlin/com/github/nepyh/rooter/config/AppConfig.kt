98bcdce1e555c34af235e395a25fe07117828fdc
nyung517 <k.youngbin.001@gmail.com>
2026-06-18 20:12:25 +0900

    add: 서버에 요구되는 설정값들을 정의하는 AppConfig 클래스 추가

---
+++ src/main/kotlin/com/github/nepyh/rooter/config/AppConfig.kt
@@ -0,0 +1,52 @@
+package com.github.nepyh.rooter.config
+
+import io.ktor.server.config.ApplicationConfig
+
+/*
+
+            jdbcUrl = environment.config.property("storage.jdbcUrl").getString(),
+            username = environment.config.property("storage.dbUser").getString(),
+            password = environment.config.property("storage.dbPassword").getString(),
+            maxPoolSize = environment.config.property("storage.dbMaxPoolSize").getString().toInt()
+ */
+
+data class AppConfig(
+    val environment: EnvironmentMode,
+
+    // database related
+    val jdbcUrl: String,
+    val dbUsername: String,
+    val dbPassword: String,
+    val dbMaxPoolSize: Int,
+
+    // cors related
+    val corsAllowedOrigins: List<String>,
+    val corsMaxAgeSeconds: Long
+) {
+    companion object {
+        fun fromApplicationConfig(config: ApplicationConfig): AppConfig {
+            val envMode = EnvironmentMode.fromString(
+                config.property("ktor.deployment.environment").getString()
+            )
+
+            val origins = config.property("cors.allowedOrigins")
+                .getString()
+                .split(",")
+                .map { it.trim() }
+                .filter { it.isNotEmpty() }
+                .map { it.removeSuffix("/") }
+
+            return AppConfig(
+                environment = envMode,
+
+                jdbcUrl = config.property("database.jdbcUrl").getString(),
+                dbUsername = config.property("database.dbUser").getString(),
+                dbPassword = config.property("database.dbPassword").getString(),
+                dbMaxPoolSize = config.property("database.dbMaxPoolSize").getString().toInt(),
+
+                corsAllowedOrigins = origins,
+                corsMaxAgeSeconds = config.property("cors.maxAgeSeconds").getString().toLong()
+            )
+        }
+    }
+}
