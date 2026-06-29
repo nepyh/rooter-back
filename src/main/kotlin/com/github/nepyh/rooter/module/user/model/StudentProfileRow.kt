a2602205e190184b7d7f2af9419561dbccfe11db
rhtnddd <qlalfql123@gmail.com>
2026-06-30 07:34:15 +0900

    edit: SchoolTable, SchoolRow참조하는거 char(10)으로 변경

--- src/main/kotlin/com/github/nepyh/rooter/module/user/model/StudentProfileRow.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/user/model/StudentProfileRow.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.v1.dao.IntEntityClass
 
 object StudentProfileTable : IntIdTable("student_profiles") {
     val user = reference("user_id", UserTable)
-    val school = reference("school_id", SchoolTable)
+    val school = char("school_id", 10)
     val grade = integer("grade")
     val classNumber = integer("class_number")
 }
@@ -16,7 +16,7 @@ class StudentProfileRow(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<StudentProfileRow>(StudentProfileTable)
 
     var user by UserRow referencedOn StudentProfileTable.user
-    var school by SchoolRow referencedOn StudentProfileTable.school
+    var school by StudentProfileTable.school
     var grade by StudentProfileTable.grade
     var classNumber by StudentProfileTable.classNumber
 }
