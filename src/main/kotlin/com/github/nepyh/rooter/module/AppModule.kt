405c5f6a9237f82f5efe1338349f751499a931ff
nyung517 <k.youngbin.001@gmail.com>
2026-07-01 19:29:39 +0900

    refactor: SwaggerMoodule -> SwaggerDocsModule 이름 변명 밎 스웨거 route 세팅을 .configureSwagger 함수 대신 ApiRoute 를 사용하도록 변경

--- src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
@@ -7,7 +7,7 @@ import com.github.nepyh.rooter.module.database.DatabaseManager
 import com.github.nepyh.rooter.module.example.exampleModule
 import com.github.nepyh.rooter.module.health.healthModule
 import com.github.nepyh.rooter.module.storage.fileStorageModule
-import com.github.nepyh.rooter.module.swagger.configureSwagger
+import com.github.nepyh.rooter.module.swagger.swaggerModule
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.koin.core.module.Module
@@ -19,7 +19,9 @@ fun AppModule(appConfig: AppConfig): Module = module {
     single { appConfig }
 
     includes(exampleModule)
+    includes(swaggerModule)
     includes(healthModule)
+
     includes(fileStorageModule)
 
     single<List<ApiRoute>> { getAll() }
@@ -47,6 +49,4 @@ fun Application.configureAppModule() {
             }
         }
     }
-
-    configureSwagger()
 }
