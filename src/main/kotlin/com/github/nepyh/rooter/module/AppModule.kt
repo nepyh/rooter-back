041b202c14e53ed16b9fbf120fca9d2240fb645b
nyung517 <k.youngbin.001@gmail.com>
2026-07-02 19:16:00 +0900

    ad: AppModule 에 팩토리 함수 형태로 바뀐 UserModule 을 포함시킴

--- src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/AppModule.kt
@@ -7,6 +7,7 @@ import com.github.nepyh.rooter.module.example.ExampleModule
 import com.github.nepyh.rooter.module.health.HealthModule
 import com.github.nepyh.rooter.module.storage.FileStorageModule
 import com.github.nepyh.rooter.module.swagger.SwaggerDocsModule
+import com.github.nepyh.rooter.module.user.UserModule
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.koin.core.module.Module
@@ -14,16 +15,21 @@ import org.koin.dsl.module
 import org.koin.ktor.ext.inject
 
 fun AppModule(appConfig: AppConfig): Module = module {
+    // dev-related modules
     if (appConfig.environment == EnvironmentMode.DEV) {
         includes(
             ExampleModule(),
             SwaggerDocsModule()
         )
     }
-
+    // infra-related modules
     includes(
         HealthModule(),
         FileStorageModule(appConfig)
+    )
+    // service-related modules
+    includes(
+        UserModule()
     )
 
     single<List<ApiRoute>> { getAll() }
