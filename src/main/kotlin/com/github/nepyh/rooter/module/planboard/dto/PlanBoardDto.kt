deb800c0ac9485b92f7fba19517823f6953aba07
Sukchan0403 <ydhkms0904@gmail.com>
2026-07-02 09:29:50 +0900

    add:플랜보드dto

---
+++ src/main/kotlin/com/github/nepyh/rooter/module/planboard/dto/PlanBoardDto.kt
@@ -0,0 +1,18 @@
+package com.github.nepyh.rooter.module.planboard.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlanBoardCreateRequest(
+    val title: String,
+    val startDate: String,  // 👈 content 지우고 이 두 줄로 교체!
+    val endDate: String
+)
+
+@Serializable
+data class PlanBoardResponse(
+    val id: Int,
+    val title: String,
+    val content: String,
+    val createdAt: String
+)
