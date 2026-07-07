6d28782efba765d3381c8cc2c9e818735ad85237
Sukchan0403 <ydhkms0904@gmail.com>
2026-07-07 17:58:30 +0900

    fix: PlanBoardApi 의 GET/POST /plan-boards 라우트에 respondCatching 적용 (예외 발생 시 500만 뜨던 문제 수정)

--- src/main/kotlin/com/github/nepyh/rooter/module/planboard/PlanBoardApi.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/planboard/PlanBoardApi.kt
@@ -7,18 +7,21 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 class PlanBoardApi(private val planBoardService: PlanBoardService) {
-    // 롤백: 원래 쓰던 대로 registerRoutes() 사용!
     fun Route.registerRoutes() {
         route("plan-boards") {
             get {
-                val boards = planBoardService.getAllBoards()
-                call.respond(status = HttpStatusCode.OK, message = boards)
+                call.respondCatching {
+                    val boards = planBoardService.getAllBoards()
+                    call.respond(status = HttpStatusCode.OK, message = boards)
+                }
             }
 
             post {
-                val request = call.receive<PlanBoardCreateRequest>()
-                planBoardService.createBoard(request)
-                call.respond(status = HttpStatusCode.Created, message = mapOf("message" to "성공적으로 등록되었습니다."))
+                call.respondCatching {
+                    val request = call.receive<PlanBoardCreateRequest>()
+                    planBoardService.createBoard(request)
+                    call.respond(status = HttpStatusCode.Created, message = mapOf("message" to "성공적으로 등록되었습니다."))
+                }
             }
         }
     }
