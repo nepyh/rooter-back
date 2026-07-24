7b2770b3a1b2df730ca2c0f201f104cea07957f6
nyung517 <k.youngbin.001@gmail.com>
2026-07-24 22:26:07 +0900

    edit: Bootstrapper.kt 에서 서비스 로직에 대한 직접적인 참조를 막기 위해, JWT 유저 인증 부분을 common 모듈의 JwtValidator 인터페이스로 분리

--- src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
+++ src/main/kotlin/com/github/nepyh/rooter/Bootstrapper.kt
@@ -2,13 +2,13 @@ package com.github.nepyh.rooter
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.github.nepyh.rooter.common.auth.JwtValidator
 import com.github.nepyh.rooter.common.config.AppConfig
 import com.github.nepyh.rooter.common.config.EnvironmentMode
 import com.github.nepyh.rooter.common.database.DatabaseConfig
 import com.github.nepyh.rooter.common.database.DatabaseManager
 import com.github.nepyh.rooter.module.AppModule
 import com.github.nepyh.rooter.module.configureAppModule
-import com.github.nepyh.rooter.module.user.UserRepo
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
@@ -30,7 +30,7 @@ fun Application.appEntryModule() {
         modules(AppModule(appConfig))
     }
 
-    val userRepo: UserRepo by inject()
+    val jwtValidator: JwtValidator by inject()
 
     install(ContentNegotiation) {
         json()
@@ -64,17 +64,7 @@ fun Application.appEntryModule() {
                     .withIssuer(appConfig.jwtIssuer)
                     .build()
             )
-            validate { credential ->
-                val userId = credential.payload.getClaim("userId").asInt()
-                val tokenVersion = credential.payload.getClaim("tokenVersion").asInt()
-                val user = userId?.let { userRepo.findUserById(it) }
-
-                if (user != null && tokenVersion == user.tokenVersion) {
-                    JWTPrincipal(credential.payload)
-                } else {
-                    null
-                }
-            }
+            validate { credential -> jwtValidator.validate(credential) }
             challenge { _, _ ->
                 call.respond(HttpStatusCode.Unauthorized, mapOf("message" to "인증이 필요합니다."))
             }
