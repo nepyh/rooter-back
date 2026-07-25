c08811e971084cde5cc9db080c59db9c479bf869
nyung517 <k.youngbin.001@gmail.com>
2026-07-25 21:48:22 +0900

    edit: S3FileStorageSpec 에서 환경변수 누락으로 인한 테스트 실패시 출력되는 메세지에 리드미 참고하라는 메세지 추가

--- src/test/kotlin/S3FileStorageSpec.kt
+++ src/test/kotlin/S3FileStorageSpec.kt
@@ -6,7 +6,8 @@ class S3FileStorageSpec : FileStorageSpec(
     beforeSetup = {
         assumeTrue(
             !System.getenv("TEST_S3_BUCKET").isNullOrBlank(),
-            "TEST_S3_BUCKET 환경 변수가 없어 S3 테스트를 건너뜁니다.",
+            "TEST_S3_BUCKET 환경 변수가 없어 S3 테스트를 건너뜁니다. " +
+                    "만약 이 에러를 봤다면 리드미의 테스트 부분 참고하삼",
         )
     },
     makeStorage = {
