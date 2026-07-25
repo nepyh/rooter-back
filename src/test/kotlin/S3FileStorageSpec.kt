d4e96c5983f8614bd0dd733fde56d856941df2d5
nyung517 <k.youngbin.001@gmail.com>
2026-07-25 21:51:32 +0900

    add: S3FileStorageSpec 의 beforeSetup 블럭에 TEST_S3_REGION 변수를 체크하는 부분 추가

--- src/test/kotlin/S3FileStorageSpec.kt
+++ src/test/kotlin/S3FileStorageSpec.kt
@@ -6,14 +6,19 @@ class S3FileStorageSpec : FileStorageSpec(
     beforeSetup = {
         assumeTrue(
             !System.getenv("TEST_S3_BUCKET").isNullOrBlank(),
-            "TEST_S3_BUCKET 환경 변수가 없어 S3 테스트를 건너뜁니다. " +
+            "TEST_S3_BUCKET 환경 변수가 없음. 3SFileStorageSpec 테스트 건너뜀. " +
+                    "만약 이 에러를 봤다면 리드미의 테스트 부분 참고하삼",
+        )
+        assumeTrue(
+            !System.getenv("TEST_S3_REGION").isNullOrBlank(),
+            "TEST_S3_REGION 환경 변수가 없음. 3SFileStorageSpec 테스트 건너뜀. " +
                     "만약 이 에러를 봤다면 리드미의 테스트 부분 참고하삼",
         )
     },
     makeStorage = {
         S3FileStorage(
-            region = System.getenv("TEST_S3_REGION") ?: "ap-northeast-2",
-            bucket = checkNotNull(System.getenv("TEST_S3_BUCKET")),
+            region = System.getenv("TEST_S3_REGION"),
+            bucket = System.getenv("TEST_S3_BUCKET"),
         )
     },
 )
